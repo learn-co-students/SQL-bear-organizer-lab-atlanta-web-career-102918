148d3c0fb3447cf6685d1bf05b5482f2035a10aa
Andrew Jones <joneseyit@protonmail.com>
2018-11-07 21:00:52 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+-- id INTEGER PRIMARY KEY,
+-- name TEXT,
+-- age INTEGER,
+-- gender TEXT,
+-- color TEXT,
+-- temperament TEXT,
+-- alive BOOLEAN
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 3, "male", "brown", "hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 3, "male", "brown", "hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Tabitha", 3, "male", "brown", "hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Sergeant Brown", 3, "female", "fuzzy", "hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Melissa", 3, "female", "fuzzy", "hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Grinch", 3, "female", "fuzzy", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Wendy", 3, "female", "brown", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)VALUES
+(NULL, 3, "female", "brown", "happy", 1);

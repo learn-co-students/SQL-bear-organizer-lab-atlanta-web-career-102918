75dc27ff2322289feb7b4185196cfb445fa68d5a
paristuzun <paristuzun@gmail.com>
2018-11-08 19:19:55 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+
 # Database
 gem 'sqlite3'
 
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "male", "brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "male", "orange", "rowdy", 1 );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "gender fluid", "pink", "sassy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "male", "dark brown", "bossy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "female", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "male", "green", "mischievous", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "female", "black", "social", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "male", "dark brown", "irratable", 0);
